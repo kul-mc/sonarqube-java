c876313bf958257eed33b6f0236a0eea91974497
Kulbhushan <kulbhushan.mayer@gmail.com>
2023-01-10 18:47:13 +0530

    Fixed - Duplicate Literals - Sonarqube

--- src/main/java/com/demo/app/validator/UserValidator.java
+++ src/main/java/com/demo/app/validator/UserValidator.java
@@ -19,17 +19,18 @@ public class UserValidator implements Validator {
 		return User.class.equals(aClass);
 	}
 
+	public static String USERNAME="username";
+
 	@Override
 	public void validate(Object o, Errors errors) {
 		User user = (User) o;
-
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username",
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, USERNAME,
 				"NotEmpty");
 		if (user.getUsername().length() < 6 || user.getUsername().length() > 32) {
-			errors.rejectValue("username", "Size.userForm.username");
+			errors.rejectValue(USERNAME, "Size.userForm.username");
 		}
 		if (userService.findByUsername(user.getUsername()) != null) {
-			errors.rejectValue("username", "Duplicate.userForm.username");
+			errors.rejectValue(USERNAME, "Duplicate.userForm.username");
 		}
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password",
